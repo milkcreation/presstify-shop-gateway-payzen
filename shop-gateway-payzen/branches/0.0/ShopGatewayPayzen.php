1b72d1d994141741be7fd4ab0a06e1defcd0133b
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-03-26 09:48:32 +0000

    shop-gateway-payzen -- branch 0.0 -- Déploiement initial

---
+++ shop-gateway-payzen/branches/0.0/ShopGatewayPayzen.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @name ShopGatewayPayzen
+ * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstify-plugins/shop-gateway-payzen
+ * @namespace \tiFy\Plugins\ShopGatewayPayzen
+ * @version 1.0.0
+ */
+
+namespace tiFy\Plugins\ShopGatewayPayzen;
+
+use tiFy\App\Plugin;
+
+final class ShopGatewayPayzen extends Plugin
+{
+
+}
