74b7e8f0aa5135ad75e06fa15ee9eb95d5396b13
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-03-29 18:02:38 +0000

    shop-gateway-payzen -- branch 0.0 -- Paiement quasiment fini reste le panier à vider au retour de la boutique

--- shop-gateway-payzen/branches/0.0/ShopGatewayPayzen.php
+++ shop-gateway-payzen/branches/0.0/ShopGatewayPayzen.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/shop-gateway-payzen
  * @namespace \tiFy\Plugins\ShopGatewayPayzen
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 namespace tiFy\Plugins\ShopGatewayPayzen;
