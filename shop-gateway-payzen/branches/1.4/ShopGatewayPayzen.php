9e005d912ea241742d3549922e277fcfdcd2278b
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-08-20 11:15:03 +0000

    shop-gateway-payzen -- unification n° version tiFY compatible 1.4

---
+++ shop-gateway-payzen/branches/1.4/ShopGatewayPayzen.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @name ShopGatewayPayzen
+ * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstify-plugins/shop-gateway-payzen
+ * @namespace \tiFy\Plugins\ShopGatewayPayzen
+ * @version 1.0.1
+ */
+
+namespace tiFy\Plugins\ShopGatewayPayzen;
+
+use tiFy\App\Plugin;
+use League\Event\Event;
+use tiFy\Plugins\Shop\Gateways\GatewaysInterface;
+
+final class ShopGatewayPayzen extends Plugin
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->appListen('tify.plugins.shop.gateways.register', [$this, 'register']);
+    }
+
+    /**
+     * Déclaration de la plateforme de paiement.
+     * @see http://event.thephpleague.com/2.0/listeners/callables/
+     *
+     * @param Event $event
+     * @param GatewaysInterface $gateways
+     *
+     * @return void
+     */
+    public function register(Event $event, GatewaysInterface $gateways)
+    {
+        $gateways->add('payzen', PayzenGateway::class);
+    }
+}
