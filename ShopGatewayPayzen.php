f7bb38fdf8ea63dc0032bebd14c6286ced6916dd
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-11-28 18:34:19 +0000

    shop-gateway-payzen -- branch 2.0 -- ajustement checkout

--- ShopGatewayPayzen.php
+++ ShopGatewayPayzen.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/shop-gateway-payzen
  * @namespace \tiFy\Plugins\ShopGatewayPayzen
- * @version 2.0.3
+ * @version 2.0.4
  */
 
 namespace tiFy\Plugins\ShopGatewayPayzen;
