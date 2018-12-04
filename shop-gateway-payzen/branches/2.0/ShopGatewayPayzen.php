106c25772e9ecbb7f1a9b090269ad9936bba10c0
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-12-04 13:45:44 +0000

    shop-gateway-payzen -- branch 2.0 -- processus commande maj status

--- shop-gateway-payzen/branches/2.0/ShopGatewayPayzen.php
+++ shop-gateway-payzen/branches/2.0/ShopGatewayPayzen.php
@@ -6,7 +6,7 @@
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstify-plugins/shop-gateway-payzen
  * @namespace \tiFy\Plugins\ShopGatewayPayzen
- * @version 2.0.4
+ * @version 2.0.5
  */
 
 namespace tiFy\Plugins\ShopGatewayPayzen;
