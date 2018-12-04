33c9b80fa65391215e3e63f8e9ba06feeb8dea8b
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-12-04 13:46:15 +0000

    shop-gateway-payzen -- tag 2.0.5 -- processus commande maj status

---
+++ shop-gateway-payzen/tags/2.0.5/ShopGatewayPayzen.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @name ShopGatewayPayzen
+ * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstify-plugins/shop-gateway-payzen
+ * @namespace \tiFy\Plugins\ShopGatewayPayzen
+ * @version 2.0.5
+ */
+
+namespace tiFy\Plugins\ShopGatewayPayzen;
+
+use tiFy\Plugins\Shop\Contracts\GatewaysInterface;
+
+final class ShopGatewayPayzen
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        events()->listen('tify.plugins.shop.gateways.register', [$this, 'register']);
+    }
+
+    /**
+     * Déclaration de la plateforme de paiement.
+     * @see http://event.thephpleague.com/2.0/listeners/callables/
+     *
+     * @param GatewaysInterface $gateways
+     * @param EventsItem $event
+     *
+     *
+     * @return void
+     */
+    public function register(GatewaysInterface $gateways)
+    {
+        $gateways->add('payzen', PayzenGateway::class);
+    }
+}
