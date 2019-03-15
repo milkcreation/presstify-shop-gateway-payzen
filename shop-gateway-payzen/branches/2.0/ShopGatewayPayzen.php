e900b7663e86257b135fdff537f27efcd7bf44aa
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-03-15 18:11:42 +0000

    shop-gateway-payzen -- branch 2.0 -- correctif maj tiFy + nettoyage

--- shop-gateway-payzen/branches/2.0/ShopGatewayPayzen.php
+++ shop-gateway-payzen/branches/2.0/ShopGatewayPayzen.php
@@ -1,18 +1,17 @@
 <?php
 
-/**
- * @name ShopGatewayPayzen
- * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy.
- * @author Jordy Manner <jordy@milkcreation.fr>
- * @package presstify-plugins/shop-gateway-payzen
- * @namespace \tiFy\Plugins\ShopGatewayPayzen
- * @version 2.0.5
- */
-
 namespace tiFy\Plugins\ShopGatewayPayzen;
 
 use tiFy\Plugins\Shop\Contracts\GatewaysInterface;
 
+/**
+ * ShopGatewayPayzen
+ *
+ * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package tiFy\Plugins\ShopGatewayPayzen
+ * @version 2.0.5
+ */
 final class ShopGatewayPayzen
 {
     /**
@@ -30,8 +29,6 @@ final class ShopGatewayPayzen
      * @see http://event.thephpleague.com/2.0/listeners/callables/
      *
      * @param GatewaysInterface $gateways
-     * @param EventsItem $event
-     *
      *
      * @return void
      */
