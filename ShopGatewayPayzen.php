62f75bbb191bb6ccdcd4650e32570d8bf451fb79
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-11-07 16:33:54 +0000

    shop-gateway-payzen -- tag 2.0.15 -- Correctif Bug logger

--- ShopGatewayPayzen.php
+++ ShopGatewayPayzen.php
@@ -1,45 +1,17 @@
-<?php
-
-/**
- * @name ShopGatewayPayzen
- * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy
- * @author Jordy Manner <jordy@milkcreation.fr>
- * @package presstify-plugins/shop-gateway-payzen
- * @namespace \tiFy\Plugins\ShopGatewayPayzen
- * @version 2.0.0
- */
+<?php declare(strict_types=1);
 
 namespace tiFy\Plugins\ShopGatewayPayzen;
 
-use League\Event\Event;
-use tiFy\Apps\AppController;
-use tiFy\Plugins\Shop\Gateways\GatewaysInterface;
-
-final class ShopGatewayPayzen extends AppController
+/**
+ * @desc Plateforme de paiement Payzen pour le plugin ecommerce Shop de PresstiFy.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package tiFy\Plugins\ShopGatewayPayzen
+ * @version 2.0.15
+ *
+ * @see https://payzen.eu/support/integration-payzen/
+ * @see https://payzen.io/fr-FR/form-payment/quick-start-guide/sitemap.html Guide d'intégration.
+ */
+class ShopGatewayPayzen
 {
-    /**
-     * CONSTRUCTEUR.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
 
-        $this->appEventListen('tify.plugins.shop.gateways.register', [$this, 'register']);
-    }
-
-    /**
-     * Déclaration de la plateforme de paiement.
-     * @see http://event.thephpleague.com/2.0/listeners/callables/
-     *
-     * @param Event $event
-     * @param GatewaysInterface $gateways
-     *
-     * @return void
-     */
-    public function register(Event $event, GatewaysInterface $gateways)
-    {
-        $gateways->add('payzen', PayzenGateway::class);
-    }
 }
