3ec0853b4065bf0d42e73de6bee39e56e4f164f1
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-06-10 15:19:43 +0000

    shop-gateway-payzen -- 2.0.20 -- Modification Réécriture shop

---
+++ shop-gateway-payzen/tags/2.0.20/Resources/views/checkout-payment-form.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @var tiFy\Plugins\Shop\Contracts\Order $order
+ * @var tiFy\Plugins\ShopGatewayPayzen\Payzen\PayzenRequest $request
+ */
+?>
+<div>
+    <p><?php _e('Merci de bien vouloir patienter, vous allez être redirigé vers la plateforme de paiement', 'tify'); ?></p>
+    <p><?php _e('Si la redirection ne s\'est pas effectuée au bout de 10 secondes, cliquer sur le bouton "valider".', 'tify'); ?></p>
+    <form method="POST" action="<?php echo $request->get('vads_platform_url'); ?>"  name="payzen_payment_form" target="_top">
+        <?php foreach($request->getHtmlFields() as $field) : ?>
+            <?php echo $field; ?>
+        <?php endforeach; ?>
+        <input type="submit" class="button-alt" id="submit_payzen_payment_form" value="<?php _e('Envoyer', 'tify'); ?>" />
+    </form>
+</div>
+<?php /* * / ?>
+<script type="text/javascript">
+    function payzen_submit_form() {
+        document.getElementById('submit_payzen_payment_form').click();
+    }
+
+    if (window.addEventListener) {
+        window.addEventListener('load', payzen_submit_form, false);
+    } else if (window.attachEvent) {
+        window.attachEvent('onload', payzen_submit_form);
+    }
+</script>
+<?php /**/
